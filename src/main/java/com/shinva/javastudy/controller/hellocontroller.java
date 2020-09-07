f0570ec7dcbbb13bb67528c8e1e9548a3b5de400
dagangge <865580807@qq.com>
2020-09-07 11:20:25 +0800

    HelloWorld

---
+++ src/main/java/com/shinva/javastudy/controller/hellocontroller.java
@@ -0,0 +1,21 @@
+package com.shinva.javastudy.controller;
+
+import cn.hutool.core.util.StrUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lrg
+ * @date 2020/9/7
+ **/
+@RestController
+public class hellocontroller {
+    @GetMapping("hello")
+    public String hello(@RequestParam(required = false,name = "who") String who) {
+        if (StrUtil.isBlank(who)) {
+            who = "world";
+        }
+        return "hello" + who;
+    }
+}
---
+++ src/test/java/com/shinva/javastudy/JavastudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinva.javastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavastudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
